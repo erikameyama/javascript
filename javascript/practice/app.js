fb32ba12b5532cb264a93ad1b0f4fd5df7d0b314
kameyama <eri.kameyama@giztech-pro.com>
2024-07-23 17:13:36 +0900

    docs: 解答終了

---
+++ javascript/practice/app.js
@@ -0,0 +1,10 @@
+let myAge = 30;
+if (myAge >= 35) {
+  console.log('健康診断でバリウムを飲みます');
+} else if (myAge >= 20) {
+  console.log('お酒が飲めます');
+} else if (myAge >= 18) {
+  console.log('選挙権があります');
+} else {
+  console.log('子供です');
+}
